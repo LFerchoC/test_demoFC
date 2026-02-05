a0383bd4b462bb875d5287fa84d58355acc26c00
lfcoelloc <lfcoelloc@gmail.com>
2026-02-04 19:17:04 -0500

    fetch album test was extended

--- test/fetch_album_test.dart
+++ test/fetch_album_test.dart
@@ -1,5 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test_demo/services/fetch_album.dart';
+
+import 'fetch_album_test.mocks.dart';
 
 // Generate a MockClient using the Mockito package.
 // Create new instances of this class in each test.
@@ -7,4 +12,32 @@ import 'package:mockito/annotations.dart';
 // `MockClient` from `package:http/testing.dart`.
 @GenerateMocks([], customMocks: [MockSpec<http.Client>(as: #MockHttpClient)])
 void main() {
+  group('fetchAlbum', () {
+    test('returns an Album if the http call completes successfully', () async {
+      final client = MockHttpClient();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(
+        client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+      ).thenAnswer(
+        (_) async =>
+            http.Response('{"userId": 1, "id": 2, "title": "mock"}', 200),
+      );
+
+      expect(await fetchAlbum(client), isA<Album>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockHttpClient();
+
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
+      when(
+        client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+      ).thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchAlbum(client), throwsException);
+    });
+  });
 }
